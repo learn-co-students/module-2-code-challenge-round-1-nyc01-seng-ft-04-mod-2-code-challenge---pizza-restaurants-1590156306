2ec24c82aa65d96d6e089f7cfbdc9533c8efdbc9
blobbyblobfish <aywang6@gmail.com>
2020-05-22 11:04:53 -0400

    Finished Deliverable 7

--- app/models/restaurant_pizza.rb
+++ app/models/restaurant_pizza.rb
@@ -2,6 +2,7 @@ class RestaurantPizza < ApplicationRecord
     belongs_to :restaurant 
     belongs_to :pizza
 
-    validates :price, numericality: {greater_than: 1}
-    validates :price, numericality: {less_than: 30}
+    validates :price, numericality: {greater_than_or_equal_to: 1}
+    validates :price, numericality: {less_than_or_equal_to: 30}
+    validates :pizza_id, uniqueness: {scope: :restaurant_id}
 end
