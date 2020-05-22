76c702f3063f45de58b8f632c760326bc7245688
jameschjiang13 <monochrome473@gmail.com>
2020-05-22 11:48:12 -0400

    Done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,12 @@
 class Restaurant < ApplicationRecord
     has_many :restaurant_pizzas
     has_many :pizzas, through: :restaurant_pizzas
+
+    def average_price
+        sum = 0
+        self.pizzas.each do |pizza|
+            sum += RestaurantPizza.find_by(restaurant_id: self.id, pizza_id: pizza.id).price
+        end
+        sum/self.pizzas.count
+    end
 end
