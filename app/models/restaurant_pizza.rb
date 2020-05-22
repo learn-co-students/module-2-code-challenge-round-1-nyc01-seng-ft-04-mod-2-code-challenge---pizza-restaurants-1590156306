38e79c15e24deaf4aa13cdd9b0303ea8d156d657
jameschjiang13 <monochrome473@gmail.com>
2020-05-22 11:56:51 -0400

    deliverables

--- app/models/restaurant_pizza.rb
+++ app/models/restaurant_pizza.rb
@@ -5,5 +5,5 @@ class RestaurantPizza < ApplicationRecord
     validates :price, numericality: { less_than_or_equal_to: 30}
     validates :price, numericality: { greater_than_or_equal_to: 1}
     validates :price, numericality: { only_integer: true }
-    validates :pizza_id, uniqueness: { scope: :restaurant_id, message: "This pizza is already on your menu!"}
+    validates :pizza_id, uniqueness: { scope: :restaurant_id, message: "This pizza is already on your menu, so let me pass the code challenge please."}
 end
