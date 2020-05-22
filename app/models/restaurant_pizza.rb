c4001cfaf422a61cdd8c288d9caa30df69bc1bdb
blobbyblobfish <aywang6@gmail.com>
2020-05-22 11:02:18 -0400

    Finished Deeliverables 3, 5 & 6

--- app/models/restaurant_pizza.rb
+++ app/models/restaurant_pizza.rb
@@ -1,4 +1,7 @@
 class RestaurantPizza < ApplicationRecord
     belongs_to :restaurant 
     belongs_to :pizza
+
+    validates :price, numericality: {greater_than: 1}
+    validates :price, numericality: {less_than: 30}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
 
   resources :pizzas, only: [:index, :show]
   resources :restaurants, only: [:index, :show]
+  resources :restaurant_pizzas, only: [:new, :create]
 end
