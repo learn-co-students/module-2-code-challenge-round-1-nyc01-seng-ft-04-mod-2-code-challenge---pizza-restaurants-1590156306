3707fd051b0e149b8f7dee1e9fd68bfb52484ad6
DarkLaughter <ramon.l.echeverria@gmail.com>
2020-05-22 11:14:50 -0400

    deliverables

--- app/controllers/restaurant_pizzas_controller.rb
+++ app/controllers/restaurant_pizzas_controller.rb
@@ -1,2 +1,24 @@
 class RestaurantPizzasController < ApplicationController
+
+    def new
+        @rp = RestaurantPizza.new
+    end
+
+    def create
+        @rp = RestaurantPizza.create(rp_params)
+        if @rp.valid?
+            redirect_to restaurant_path(@rp.restaurant_id)
+        else
+            flash[:errors] = @rp.errors.full_messages
+            redirect_to new_restaurant_pizza_path
+        end
+    end
+
+
+
+    private 
+    def rp_params
+        params.require(:restaurant_pizza).permit(:restaurant_id, :pizza_id, :price)
+    end
+    
 end
--- app/models/restaurant_pizza.rb
+++ app/models/restaurant_pizza.rb
@@ -1,4 +1,6 @@
 class RestaurantPizza < ApplicationRecord
   belongs_to :restaurant
   belongs_to :pizza
+
+  validates :price, numericality: { greater_than: 0, less_than: 31, message: "must be between 1 and 30" }
 end
