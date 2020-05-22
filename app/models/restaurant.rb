a912e63487f8d7c8ac9488574c792a6c97b9d9ea
DarkLaughter <ramon.l.echeverria@gmail.com>
2020-05-22 11:39:34 -0400

    safety commit

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,8 +2,10 @@ class Restaurant < ApplicationRecord
     has_many :restaurant_pizzas
     has_many :pizzas, through: :restaurant_pizzas
 
-    # def self.total_restaurants
-    #     self.count
-    # end
+    def avg_price
+        total = self.restaurant_pizzas.map {|pizza| pizza.price}.sum
+        avg = total/self.restaurant_pizzas.count
+        avg.to_f
+    end
   
 end
