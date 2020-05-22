f425819d148862dfe659d2b313cc9e3328a09acd
carolineglass <carolineglass89@gmail.com>
2020-05-22 11:57:01 -0400

    deliverables

--- app/models/pizza.rb
+++ app/models/pizza.rb
@@ -1,4 +1,8 @@
 class Pizza < ApplicationRecord
     has_many :restaurant_pizzas
     has_many :restaurants, through: :restaurant_pizzas
+
+    def num_of_restaurants
+        self.restaurants.count
+    end 
 end
