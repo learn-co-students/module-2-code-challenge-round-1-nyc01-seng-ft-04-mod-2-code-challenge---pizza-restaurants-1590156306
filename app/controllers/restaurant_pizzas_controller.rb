fa68d81524bea5d073252210e601739afb4f56e1
carolineglass <carolineglass89@gmail.com>
2020-05-22 11:46:18 -0400

    mvp

--- app/controllers/restaurant_pizzas_controller.rb
+++ app/controllers/restaurant_pizzas_controller.rb
@@ -4,7 +4,19 @@ class RestaurantPizzasController < ApplicationController
     end
   
     def create
-      
+      @restaurant_pizza = RestaurantPizza.create(allowed_params)
+        if @restaurant_pizza.valid?
+            redirect_to restaurant_path(@restaurant_pizza.restaurant.id)
+        else
+            flash[:errors] = @restaurant_pizza.errors.full_messages
+            redirect_to new_restaurant_pizza_path
+        end
     end
+
+    private
+
+    def allowed_params
+        params.require(:restaurant_pizza).permit(:pizza_id, :restaurant_id, :price)
+    end 
   
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :restaurants, only: [:index, :show]
   resources :pizzas, only: [:index, :show]
-  resources :restauraunt_pizzas, only: [:new, :create]
+  resources :restaurant_pizzas, only: [:new, :create]
 end
