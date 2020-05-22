fc2f5442414bf5103741dc9988f25ebd8afe183c
Kyle Tomanelli <kyletomanelli@gmail.com>
2020-05-22 11:17:43 -0400

    11:17

--- app/models/pizza.rb
+++ app/models/pizza.rb
@@ -1,4 +1,7 @@
 class Pizza < ApplicationRecord
     has_many :restaurant_pizzas
     has_many :restaurants,through: :restaurant_pizzas
+    def rest_count
+        self.restaurant_pizzas.length
+    end
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,12 @@
 class Restaurant < ApplicationRecord
   has_many :restaurant_pizzas
   has_many :pizzas,through: :restaurant_pizzas
+
+
+  def price_avg
+    my_sum = self.restaurant_pizzas.sum do |pizza|
+      pizza.price
+    end
+    avg=my_sum/self.pizzas.length
+  end
 end
