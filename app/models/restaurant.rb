e1bfe816a166d4dea8263c197b223420cf05d8b7
Jessica <github email address>
2020-05-22 11:42:19 -0400

    adjusted average method

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,9 +3,7 @@ class Restaurant < ApplicationRecord
     has_many :pizzas, through: :restaurant_pizzas
 
     def average_price
-        total_price = self.restaurant_pizzas.map do |pizza|
-            pizza.price 
-        end.sum
+        total_price = self.restaurant_pizzas.map {|pizza| pizza.price}.sum
         number_of_pizza = self.restaurant_pizzas.count 
         (total_price/number_of_pizza).to_f
     end
