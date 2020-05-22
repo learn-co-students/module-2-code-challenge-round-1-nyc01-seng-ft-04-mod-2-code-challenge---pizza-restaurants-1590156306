4e0e5d717934d9c1b6cdb57e3de7c7777f853fa6
Joshua <jykim92@gmail.com>
2020-05-22 11:43:08 -0400

    Updated Advanced Validations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,4 +2,13 @@ class Restaurant < ApplicationRecord
   
     has_many :restaurant_pizzas
     has_many :pizzas, through: :restaurant_pizzas
+
+    def average_price
+        total = self.pizzas.count
+        total_price = 0
+        self.restaurant_pizzas.each do |x|
+            total_price += x.price
+        end
+        total_price/total
+    end
 end
--- app/models/restaurant_pizza.rb
+++ app/models/restaurant_pizza.rb
@@ -4,6 +4,7 @@ class RestaurantPizza < ApplicationRecord
     belongs_to :restaurant
 
     validates :price, numericality: { greater_than: 1, less_than: 30, message: "must be between 1 and 30"}
+    validates :pizza_id, uniqueness: { scope: :restaurant_id, message: "already exists on the menu. Please add a new item"}
 
 
 end
